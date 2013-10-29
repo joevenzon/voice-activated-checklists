a399d05b0e39886108ec73092643cc30201a0829
endever@gmail.com <endever@gmail.com@fa93f969-12fe-4c33-9b94-d61b6a6942ed>
2013-10-29 20:22:09 +0000

    git-svn-id: svn://localhost/tmp/vac/trunk@18 fa93f969-12fe-4c33-9b94-d61b6a6942ed

---
+++ SimVoiceChecklist/StatusWindow.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace SimVoiceChecklists
+{
+    partial class StatusWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblText = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.ForeColor = System.Drawing.Color.White;
+            this.lblText.Location = new System.Drawing.Point(5, 3);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(406, 14);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StatusWindow
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(423, 20);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblText);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(20, 50);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StatusWindow";
+            this.Opacity = 0.65D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "StatusWindow";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
