fed5ff6fc1e587687a90d8b41189e0b21d412552
endever@gmail.com <endever@gmail.com@fa93f969-12fe-4c33-9b94-d61b6a6942ed>
2013-10-29 20:22:00 +0000

    git-svn-id: svn://localhost/tmp/vac/trunk@16 fa93f969-12fe-4c33-9b94-d61b6a6942ed

---
+++ SimVoiceChecklist/SpeechProcessing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+
+namespace SimVoiceChecklists
+{
+    class SpeechProcessing
+    {
+        public string ProcessRecognitionResult(RecognitionResult VoiceResult)
+        {
+            string result = "";
+
+            return result;
+        }
+    }
+}
